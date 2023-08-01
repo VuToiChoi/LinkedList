3017b216441abc70c12dd32853df7fee7ba92b29
VuToiChoi <138584418+VuToiChoi@users.noreply.github.com>
2023-08-01 11:39:37 +0200

    LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -8,12 +8,14 @@ using namespace std;
 1. Add//
 2. Add first //
 3. Add Last//
-4. Remove first
-5. Remove Last
-6. Remove at slot I
-7. Get first Node Data
-8. Contains = innehålla (om det finns data i listans)
-9. Edit node data at index
+3.5 Edit At index//
+4. Remove first//
+5. Remove Last//
+6. Remove at slot I//
+7. Remove All//
+8. Get Data at index//
+9. Contains = innehålla (om det finns data i listans)
+10. Edit node data at index
 */
 
 struct Node
@@ -68,6 +70,36 @@ struct LinkedList
         }
     }
 
+    void EditAtIndex(int data, int index)
+    {
+        Node* current_ = head_;
+        bool outOfBound = true;
+        if (head_ == nullptr)
+        {
+            cout << "noob" << endl;
+        }
+        else
+        {
+            for (int i = 0; i < index; i++)
+            {
+                if (current_->next_ == nullptr)
+                {
+                    cout << "Index out of bound";
+                    outOfBound = false;
+                    break;
+                }
+                else
+                {
+                    current_ = current_->next_;
+                }
+            }
+            if(outOfBound)
+            {
+                current_->data_ = data;
+            }
+        }
+    }
+
     void AddAtIndex(int data, int index)
     {
         Node* current_ = head_;
@@ -77,9 +109,9 @@ struct LinkedList
         {
             AddFirst(data);
         }
-        else if (head_ != nullptr)
+        else
         {
-            for (int i = 0; i < index-1; i++)
+            for (int i = 0; i <index-1; i++) 
             {
                 if (current_->next_ == nullptr)
                 {
@@ -125,7 +157,153 @@ struct LinkedList
             }
         }
     }
-    
+    void RemoveData(int data)
+    {
+        Node* current_ = head_;
+        Node* prev_;
+
+        if (head_ == nullptr)
+        {
+            cout << "List Empty";
+        }
+        else if (head_->data_ == data)
+        {
+            head_ = current_->next_;
+            delete current_;
+        }
+        else
+        {
+            while (current_->next_ != nullptr) 
+            {
+                prev_ = current_;
+                current_ = current_->next_;
+
+                if (current_->data_ == data)
+                {
+                    prev_->next_ = current_->next_;
+                    delete current_;
+                    break;
+                }
+            }
+        }
+    }
+    void RemoveFirst()
+    {
+        Node* current_ = head_;
+        Node* prev_; 
+
+        if (head_ == nullptr)
+        {
+            cout << "List Empty";
+        }
+        else
+        {
+            head_ = current_->next_;
+            delete current_;
+        }
+    }
+
+    void RemoveLast ()
+    {
+        Node* current_ = head_;
+        Node* prev_;
+
+        while (current_->next_ != nullptr)
+        {
+            prev_ = current_;
+            current_ = current_->next_;
+
+            if (current_->next_ == nullptr)
+            {
+                prev_->next_ = current_->next_;
+                delete current_;
+                break;
+            }
+        }
+    }
+
+    void RemoveAll ()
+    {
+        Node* current_ = head_;
+        while (head_->next_ != nullptr)
+        {
+            Node* prev_;
+            prev_ = current_;
+            current_ = current_->next_;
+            delete prev_;
+            if (current_->next_ == nullptr)
+            {
+                head_ = nullptr;
+                break;
+            }
+        }
+    }
+
+    void GetDataAtIndex(int index)
+    {
+        Node* current_ = head_;
+
+        if (index == 0)
+        {
+            cout << "Data at index " << index << " is " << current_->data_ << endl;
+        }
+        else
+        {
+            for (int i = 0; i <= index; i++)
+            {
+                if (i == index)
+                {
+                    cout << "Data at index " << index << " is " << current_->data_ << endl;
+                }
+                else
+                {
+                    current_ = current_->next_;
+                }
+            }
+        }
+    }
+
+    void Contains(int data)
+    {
+        Node* current_ = head_;
+        int i = 0;
+
+        while (head_ != nullptr)
+        {
+            if (current_->data_ == data)
+            {
+                if (current_ == nullptr)
+                {
+                    cout << "Data " << current_->data_ << " found at slot " << i << endl;
+                    break;
+                }
+                else if (current_ != nullptr)
+                {
+                    cout << "Data " << current_->data_ << " found at slot " << i << endl;
+                    i = i + 1;
+                    current_ = current_->next_;
+                }
+            }
+            else if (current_->data_ != data)
+            {
+                if (current_->next_ != nullptr)
+                {
+                    i = i + 1;
+                    current_ = current_->next_;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            else if (current_->next_ == nullptr)
+            {
+                cout << "Data not found";
+                break;
+            }
+        }
+    }
+
     Node* CheckEmptyList(int data)
     {
         Node* newNode = new Node(data);
@@ -139,21 +317,27 @@ struct LinkedList
 
     void PrintList()
     {
-        Node* current_ = head_;
-
-        while (current_ != nullptr)
-        {
-            cout << current_->data_ << endl;
-
-            if (current_->next_ == nullptr)
+            Node* current_ = head_;
+            if (current_ == nullptr)
             {
-                break;
+                cout << "List empty";
             }
             else
             {
-                current_ = current_->next_;
+                while (head_ != nullptr)
+                {
+                    cout << current_->data_ << endl;
+
+                    if (current_->next_ == nullptr)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        current_ = current_->next_;
+                    }
+                }
             }
-        }
     }
 
     void Clear()
@@ -169,13 +353,11 @@ int main()
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF); //Debug-flaggor för att upptäcka minnesläckor
 
     LinkedList linkedList;
-    linkedList.AddLast(10);
     linkedList.Add(5);
-    linkedList.Add(6);
-    linkedList.Add(7);
-    linkedList.AddAtIndex(8, 2);
-    linkedList.AddAtIndex(9, 9);
-    linkedList.AddFirst(12);
+    linkedList.Add(0);
+    linkedList.Add(0);
+    linkedList.Add(0);
+    linkedList.Add(9);
     linkedList.PrintList();
-
+    linkedList.Contains(7);
 }
